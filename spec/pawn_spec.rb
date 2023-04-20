f26668aa93b53304a388592fbf1a6a8cb1299196
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-20 13:38:18 +0300

    Add testing for moves that are sometiems legal

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -9,7 +9,12 @@ describe Pawn do
 
         it "returns false when the new destination given isn't available" do
             pawn = Pawn.new([7, 1])
-            expect(pawn.move([0, 2])).to eq((false))
+            expect(pawn.move([0, 2])).to eq(false)
+        end
+
+        it "returns false when you try to move a pawn diagonally without taking" do
+            pawn = Pawn.new([5, 1])
+            expect(pawn.move([6,2])).to eq(false)
         end
     end
 end
