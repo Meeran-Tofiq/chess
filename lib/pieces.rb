697f919671f6281a2526f3d3ddefa5b579175564
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-20 13:30:26 +0300

    Feat Pieces class, Pawn class inherits from it

---
+++ lib/pieces.rb
@@ -0,0 +1,23 @@
+class Piece
+    attr_reader :pos, :symbol, :t
+    def initialize(pos, transformations = nil, symbol = "♙")
+        @pos = pos
+        @t = transformations
+        @symbol = symbol
+    end
+
+    def move(des)
+        t.each do |t|
+            new_pos = [pos[0] + t[0], pos[1] + t[1]]
+            new_pos == des ? (return (pos = new_pos)) : next
+        end
+    end
+end
+
+class Pawn < Piece
+    T = [[0, 1], [1, 1], [-1, 1]].freeze
+    def initialize(pos)
+        super(pos, transformations=T)
+    end
+end
+
