b9adde0dab4de5845f134125618fbf495efa37ad
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-21 13:18:09 +0300

    Feat .set_position method
    This method sets the sets the position provided to be a symbol. The sym-
    bol represents a piece.

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,11 @@ class Board
         @@layout = Array.new(8) {Array.new(8, nil)}
     end
 
-    def taken?(pos)
+    def self.taken?(pos)
         !@@layout[pos[0]][pos[1]].nil?
+    end
+
+    def self.set_position(pos, symbol)
+        @@layout[pos[0]][pos[1]] = symbol
     end
 end
