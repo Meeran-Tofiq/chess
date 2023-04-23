137acc8c326370ad51751dd8b95baa52ef3d36ac
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-23 14:53:30 +0300

    Refactor tests of pawn moves to flip x and y
    The x and y of each pawn was set incorrectly from the get go, now it's
    fixed.

--- spec/advanced_movement_spec.rb
+++ spec/advanced_movement_spec.rb
@@ -5,14 +5,14 @@ describe Pawn do
     describe "#move" do
         it "allows pawn to move 2 spaces, if it's its first move" do
             Board.reset_board
-            p = Pawn.new([0, 1], "w")
-            expect(p.move([0, 3])).to eq([0, 3])
+            p = Pawn.new([1, 0], "w")
+            expect(p.move([3, 0])).to eq([3, 0])
         end
 
         it "doesn't allow pawn to move 2 spaces, after its first move" do
             p = Pawn.new([1,1], "w")
-            p.move([1,2])
-            expect(p.move([1, 4])).to eq(false)
+            p.move([2,1])
+            expect(p.move([4, 1])).to eq(false)
             Board.reset_board
         end
     end
--- spec/basic_movement_spec.rb
+++ spec/basic_movement_spec.rb
@@ -3,18 +3,18 @@ require 'pieces.rb'
 describe Pawn do
     describe "#move" do
         it "moves the pawn when passed a correct space for movement" do
-            pawn = Pawn.new([0, 1], "w")
-            expect(pawn.move([0, 2])).to eq([0, 2])
+            pawn = Pawn.new([1, 0], "w")
+            expect(pawn.move([2, 0])).to eq([2, 0])
         end
         
         it "returns false when the new destination given isn't available" do
-            pawn = Pawn.new([7, 1], "w")
-            expect(pawn.move([0, 2])).to eq(false)
+            pawn = Pawn.new([1, 7], "w")
+            expect(pawn.move([2, 0])).to eq(false)
         end
         
         it "returns false when you try to move a pawn diagonally without taking" do
-            pawn = Pawn.new([5, 1], "w")
-            expect(pawn.move([6,2])).to eq(false)
+            pawn = Pawn.new([1, 5], "w")
+            expect(pawn.move([2, 6])).to eq(false)
         end
     end
 end
