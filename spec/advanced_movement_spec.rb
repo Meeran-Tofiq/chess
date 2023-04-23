bf2492dd521043f4241acaa967810859bd8b7d59
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-23 14:37:47 +0300

    Add testing for pawn double space moves

---
+++ spec/advanced_movement_spec.rb
@@ -0,0 +1,18 @@
+require 'pieces.rb'
+
+Board.reset_board
+
+describe Pawn do
+    describe "#move" do
+        it "allows pawn to move 2 spaces, if it's its first move" do
+            p = Pawn.new([0, 1], "w")
+            expect(p.move([0, 3])).to eq([0, 3])
+        end
+
+        it "doesn't allow pawn to move 2 spaces, after it's first move" do
+            p = Pawn.new([1,1], "w")
+            p.move([1,2])
+            expect(p.move([1, 4])).to eq(false)
+        end
+    end
+end
