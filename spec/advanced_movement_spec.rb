0ca248731489c0ff03da8194c9016ba34d02f8dc
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-23 14:46:24 +0300

    Refactor code to reset board often

--- spec/advanced_movement_spec.rb
+++ spec/advanced_movement_spec.rb
@@ -1,18 +1,19 @@
 require 'pieces.rb'
 
-Board.reset_board
 
 describe Pawn do
     describe "#move" do
         it "allows pawn to move 2 spaces, if it's its first move" do
+            Board.reset_board
             p = Pawn.new([0, 1], "w")
             expect(p.move([0, 3])).to eq([0, 3])
         end
 
-        it "doesn't allow pawn to move 2 spaces, after it's first move" do
+        it "doesn't allow pawn to move 2 spaces, after its first move" do
             p = Pawn.new([1,1], "w")
             p.move([1,2])
             expect(p.move([1, 4])).to eq(false)
+            Board.reset_board
         end
     end
 end
