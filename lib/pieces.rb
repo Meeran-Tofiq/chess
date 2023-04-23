1ea37ef2e3467eb419cc0c17871a503d82549227
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-23 14:20:07 +0300

    Feat new move method, better than previous
    This new and improved move method won't let you move if there is another
    piece in the way of you reaching the specified location.

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -12,6 +12,37 @@ class Piece
     def move(des)
         return false if Board.taken?(des)
         t.each do |t|
+            new_pos = pos
+            7.times do |_|
+                new_pos = [new_pos[0]+t[0], new_pos[1]+t[1]]
+                
+                break if Board.out_of_bounds?(new_pos) || Board.taken?(new_pos)
+                
+                if new_pos == des
+                    Board.set_position(new_pos, symbol)
+                    Board.set_empty(pos)
+                    return (pos = new_pos)
+                end
+            end 
+        end
+        false
+    end
+
+end
+
+class Pawn < Piece
+    
+    attr_reader :pos, :symbol
+    def initialize(pos, side)
+        @t = [[0, 1], [1, 1], [-1, 1]]
+        @symbol = (side == "w" ?  "♙" : "♟︎")
+        super(pos, @t[0..0], symbol)
+        Board.set_position(pos, symbol)
+    end
+
+    def move(des)
+        return false if Board.taken?(des)
+        t[0..0].each do |t|
             new_pos = [pos[0] + t[0], pos[1] + t[1]]
 
             if new_pos == des
@@ -22,87 +53,88 @@ class Piece
         end
         false
     end
-
-end
-
-class Pawn < Piece
-    @@t = [[0, 1], [1, 1], [-1, 1]]
-    
-    attr_reader :pos, :symbol
-    def initialize(pos, side)
-        @symbol = (side == "w" ?  "♙" : "♟︎")
-        super(pos, @@t[0..0], symbol)
-        Board.set_position(pos, symbol)
-    end
 end
 
 class Bishop < Piece
-    @@t = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
-    @@t[0..4].each do |t|
-        p = t
-        7.times { p = [p[0] + t[0], p[1] + t[1]]; @@t << p}
-    end
     
     attr_reader :pos, :symbol
     def initialize(pos, side)
+        @t = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
         @symbol = (side == "w" ?  "♗" : "♝")
-        super(pos, @@t, symbol)
+        super(pos, @t, symbol)
         Board.set_position(pos, symbol)
     end
 
-    def self.t
-        @@t
-    end
 end
 
 class Knight < Piece
-    @@t = [[1, 2], [2, 1], [-1, 2], [2, -1], [-1, -2], [-2, -1], [1, -2], [-2, 1]]
     
-    attr_reader :pos, :symbol
+    attr_reader :pos, :symbol, :t
     def initialize(pos, side)
+        @t = [[1, 2], [2, 1], [-1, 2], [2, -1], [-1, -2], [-2, -1], [1, -2], [-2, 1]]
         @symbol = (side == "w" ?  "♘" : "♞")
-        super(pos, @@t, symbol)
+        super(pos, @t, symbol)
         Board.set_position(pos, symbol)
+    end
+
+    def move(des)
+        return false if Board.taken?(des)
+        t.each do |t|
+            new_pos = [pos[0] + t[0], pos[1] + t[1]]
+
+            if new_pos == des
+                Board.set_position(new_pos, symbol)
+                Board.set_empty(pos)
+                return (pos = new_pos)
+            end
+        end
+        false
     end
 end
 
 class Rook < Piece
-    @@t = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    @@t[0..4].each do |t|
-        p = t
-        7.times { p = [p[0] + t[0], p[1] + t[1]]; @@t << p}
-    end
-
-    attr_reader :pos, :symbol
+    
+    attr_reader :pos, :symbol, :t
     def initialize(pos, side)
+        @t = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         @symbol = (side == "w" ?  "♖" : "♜")
-        super(pos, @@t, symbol)
+        super(pos, @t, symbol)
         Board.set_position(pos, symbol)
-    end
-
-    def self.t
-        @@t
     end
 end
 
 class Queen < Piece
-    @@t = Rook.t + Bishop.t
-
-    attr_reader :pos, :symbol
+    
+    attr_reader :pos, :symbol, :t
     def initialize(pos, side)
+        @t = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [-1, 1], [-1, -1], [1, -1]]
         @symbol = (side == "w" ?  "♕" : "♛")
-        super(pos, @@t, symbol)
+        super(pos, @t, symbol)
         Board.set_position(pos, symbol)
     end
 end
 
 class King < Piece
-    @@t = [[1, 0], [0, 1], [1, 1], [0, -1], [-1, 0], [-1, -1], [-1, 1], [1, -1]]
     
-    attr_reader :pos, :symbol
+    attr_reader :pos, :symbol, :t
     def initialize(pos, side)
+        @t = [[1, 0], [0, 1], [1, 1], [0, -1], [-1, 0], [-1, -1], [-1, 1], [1, -1]]
         @symbol = (side == "w" ?  "♔" : "♚")
-        super(pos, @@t, symbol)
+        super(pos, @t, symbol)
         Board.set_position(pos, symbol)
+    end
+
+    def move(des)
+        return false if Board.taken?(des)
+        t.each do |t|
+            new_pos = [pos[0] + t[0], pos[1] + t[1]]
+
+            if new_pos == des
+                Board.set_position(new_pos, symbol)
+                Board.set_empty(pos)
+                return (pos = new_pos)
+            end
+        end
+        false
     end
 end
