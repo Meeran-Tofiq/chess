9d4493303899e217090ee357ccf2fff83ef6caeb
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-06-02 16:47:46 +0300

    Add test to ensure correct black piece placement

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,9 @@ require 'player.rb'
 describe Player do
     describe "#create_pieces" do
         it "Sets the board correctly for white" do
+            Board.reset
             player = Player.new()
+            player_black = Player.new("b", false)
             Board.print
             Board.reset
         end
