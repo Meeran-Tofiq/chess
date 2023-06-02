73b3ac829f7aa0b33c960be2ad044021669c4816
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-06-02 16:46:13 +0300

    Fix bug where all the pieces craeted were pawns

--- lib/player.rb
+++ lib/player.rb
@@ -12,11 +12,11 @@ class Player
         h = Hash.new(6)
 
         h[:P] = create_piece_type(Pawn, second_line, [0,1,2,3,4,5,6,7])
-        h[:Q] = create_piece_type(Pawn, first_line, [3])
-        h[:K] = create_piece_type(Pawn, first_line, [4])
-        h[:R] = create_piece_type(Pawn, first_line, [0, 7])
-        h[:N] = create_piece_type(Pawn, first_line, [1, 6])
-        h[:B] = create_piece_type(Pawn, first_line, [2, 5])
+        h[:Q] = create_piece_type(Queen, first_line, [3])
+        h[:K] = create_piece_type(King, first_line, [4])
+        h[:R] = create_piece_type(Rook, first_line, [0, 7])
+        h[:N] = create_piece_type(Knight, first_line, [1, 6])
+        h[:B] = create_piece_type(Bishop, first_line, [2, 5])
     end
 
     def create_piece_type(p_class, row, col)
