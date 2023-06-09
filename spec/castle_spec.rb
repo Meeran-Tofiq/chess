7881da0698a21cd33fe33fcb5d764d34a2b18cd7
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-06-09 13:45:42 +0300

    Add testing for castling queen's side

--- spec/castle_spec.rb
+++ spec/castle_spec.rb
@@ -3,6 +3,7 @@ require 'player.rb'
 describe Player do
     describe "#castle" do
         it "Castles king side when the requirements of castling is met" do
+            Board.reset
             player = Player.new
             des_arr = [[3, 4], [3, 2], [2, 5]]
             piece_arr = [:P, :B, :N]
@@ -12,6 +13,21 @@ describe Player do
             end
             Board.print
             expect(player.castle(1)).to eq(true)
+        end
+
+        it "Castles queen side when the requirements are met" do
+            Board.reset
+            player = Player.new
+            des_arr = [[3, 3], [3, 5], [2, 2], [1, 3]]
+            piece_arr = [:P, :B, :N, :Q]
+            des_arr.length.times do |i|
+                puts i
+                choice = player.get_pieces_with_des(des_arr[i], piece_arr[i], false)[0]
+                choice.move(des_arr[i])
+            end
+            expect(player.castle(-1)).to eq(true)
+            puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            Board.print
         end
     end
 end
